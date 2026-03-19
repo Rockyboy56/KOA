58ca530f05fc85eb58857ed5cc6608aeb48292f8
Rockyboy56 <lucatheninja@gmail.com>
2026-03-19 15:35:18 +1300

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'assets',
+  base: '/KOA/',
   server: { port: 3000 },
   build: { outDir: 'dist' }
 });
